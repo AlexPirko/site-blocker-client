cc064e66612b3a10fe1e283b5728676582bdca1d
Алекс Южный <youzhny.foto@gmail.com>
2023-11-22 15:23:38 +0200

    add ui components

--- src/shared/api/api-instance.ts
+++ src/shared/api/api-instance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError, AxiosRequestConfig } from "axios";
 
 export const apiInstance = axios.create({
-  baseURL: "*/api",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json",
   },
